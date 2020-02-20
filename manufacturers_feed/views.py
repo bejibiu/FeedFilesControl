ceb912223214d51c40d683901f30d8a6cedddb7e
6ejibiu <akimovpnz@live.com>
2020-02-21 00:21:56 +0300

    add post handler

--- manufacturers_feed/views.py
+++ manufacturers_feed/views.py
@@ -1,7 +1,9 @@
-from http.client import HTTPResponse
-
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
+    if request.method == 'POST':
+        data = request.POST
+        return HttpResponse(data['brand_name'])
     return render(request, 'manufacturers_feed/index.html')
