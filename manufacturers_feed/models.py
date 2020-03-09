5daad522bafef73b8a326381887bf5d3f02070d1
6ejibiu <akimovpnz@live.com>
2020-03-09 23:00:24 +0300

    add test save feed in files brand

--- manufacturers_feed/models.py
+++ manufacturers_feed/models.py
@@ -7,6 +7,11 @@ class Brand(models.Model):
     name = models.CharField(max_length=BRAND_LEN)
 
 
+def get_feed_dir(instance, filename):
+    return f'{instance.brand.name}/{filename}'
+
+
 class Feed(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    feed_file = models.FileField(upload_to=brand.name)
+    feed_file = models.FileField(upload_to=get_feed_dir)
+
--- tests/manufacturers_feed/test_models.py
+++ tests/manufacturers_feed/test_models.py
@@ -1,7 +1,6 @@
 import pytest
 from django.core.files.uploadedfile import SimpleUploadedFile
 
-from FeedFilesControl.settings import base as settings
 from manufacturers_feed.models import Brand, Feed
 
 
@@ -13,9 +12,12 @@ def test_create_brand_with_name():
 
 @pytest.mark.django_db
 def test_create_feed_models(tmp_path):
-    brand_ = Brand()
+    brand_ = Brand(name="Legas")
     brand_.save()
-    feed = Feed(brand_, SimpleUploadedFile('test-bosh_file', b'example'))
+    feed = Feed()
+    feed.brand = brand_
+    feed.feed_file = SimpleUploadedFile('test-bosh_file', b'example')
     feed.save()
-    print(settings.MEDIA_ROOT)
-    assert feed.feed_file
+    saved_feed = Feed.objects.all()
+    assert saved_feed.count() == 1
+    assert brand_.name in saved_feed.first().feed_file.path
